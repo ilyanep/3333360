ebd4f4f9b92542a2ae64fc362b9b67551d021d47
Klarkshroder <pukebin@gmail.com>
2010-10-28 22:46:43 -0700

    BFS Version 2

--- src/superGreedy.py
+++ src/superGreedy.py
@@ -6,12 +6,17 @@ class superGreedyAI():
         self.path = ""
         self.mapHeight = 0
         self.mapWidth = 0
+        self.target = (0,0)
         for k,v in inputMap.iteritems():
             y, x = k
             if y > self.mapHeight:
                 self.mapHeight = y
             if x > self.mapWidth:
                 self.mapWidth = x
+            if v == 20:
+                print '20 is the value of ', k
+            elif v == 21:
+                print '21 is the value of ', k
         
     def think(self, curMap, curPos, rivalPos, curGraceTime):
         y, x = curPos
@@ -36,8 +41,8 @@ class superGreedyAI():
         if len(dotDirs) > 0:
             return random.choice(dotDirs)
         else:
-            if len(self.path) == 0:
-                self.path = self.nearestDot(x, y, curMap)
+            if len(self.path) == 0 or curMap[self.target] == 0:
+                self.path, self.target = self.nearestDot(x, y, curMap)
             print 'Path is ', self.path
             move = self.path[0]
             self.path = self.path[1:]
@@ -52,30 +57,46 @@ class superGreedyAI():
             newFrontier = {};
             for k,v in frontier.iteritems():
                 yy, xx = k
-                right = curMap[(yy, xx+1)]
-                if right == 2:
-                    return v + 'R'
-                elif right == 0 and not visited[yy][xx+1]:
-                    visited[yy][xx+1] = 1
-                    newFrontier[(yy,xx+1)] = v + 'R'
-                left = curMap[(yy, xx-1)]
-                if left == 2:
-                    return v + 'L'
-                elif left == 0 and not visited[yy][xx-1]:
-                    visited[yy][xx-1] = 1
-                    newFrontier[(yy, xx-1)] = v + 'L'
-                up = curMap[(yy-1, xx)]
-                if up == 2:
-                    return v + 'U'
-                elif up == 0:
-                    visited[yy-1][xx] = 1
-                    newFrontier[(yy-1,xx)] = v + 'U'
-                down = curMap[(yy+1,xx)]
-                if down == 2:
-                    return v + 'D'
-                elif down == 0:
-                    visited[yy+1][xx] = 1
-                    newFrontier[(yy+1,xx)] = v + 'D'
+                if xx < self.mapWidth:
+                    right = curMap[(yy, xx+1)]
+                    if right == 2:
+                        return (v + 'R', (yy, xx+1))
+                    elif right == 0 and not visited[yy][xx+1]:
+                        visited[yy][xx+1] = 1
+                        newFrontier[(yy,xx+1)] = v + 'R'
+                    elif right == 20 and not visited[yy][0]:
+                        visited[yy][0] = 1
+                        newFrontier[(yy,0)] = v + 'R'
+                if xx > 0:
+                    left = curMap[(yy, xx-1)]
+                    if left == 2:
+                        return (v + 'L', (yy, xx-1))
+                    elif left == 0 and not visited[yy][xx-1]:
+                        visited[yy][xx-1] = 1
+                        newFrontier[(yy, xx-1)] = v + 'L'
+                    elif left == 20 and not visited[yy][self.mapWidth-1]:
+                        visited[yy][self.mapWidth-1] = 1
+                        newFrontier[(yy, self.mapWidth-1)] = v + 'L'
+                if yy > 0:
+                    up = curMap[(yy-1, xx)]
+                    if up == 2:
+                        return (v + 'U', (yy-1, xx))
+                    elif up == 0:
+                        visited[yy-1][xx] = 1
+                        newFrontier[(yy-1,xx)] = v + 'U'
+                    elif up == 21 and not visited[self.mapHeight-1][xx]:
+                        visited[self.mapHeight-1][xx] = 1
+                        newFrontier[(self.mapHeight-1,xx)] = v + 'U'
+                if yy < self.mapHeight:
+                    down = curMap[(yy+1,xx)]
+                    if down == 2:
+                        return (v + 'D', (yy+1,xx))
+                    elif down == 0:
+                        visited[yy+1][xx] = 1
+                        newFrontier[(yy+1,xx)] = v + 'D'
+                    elif down == 21 and not visited[0][xx]:
+                        visited[0][xx] = 1
+                        newFrontier[(0, xx)] = v + 'D'
             frontier = newFrontier
                 
                 
