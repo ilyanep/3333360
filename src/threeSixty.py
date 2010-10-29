a2b1c17680cc601718e14f87f0715f11060cd7b5
Ilya Nepomnyashchiy <ilyanep@gmail.com>
2010-10-28 20:04:12 -0700

    Slowwwwwwww floyd-warshall implementation

--- src/threeSixty.py
+++ src/threeSixty.py
@@ -6,6 +6,64 @@ class threeSixtyAI ():
         self.numMoves = 0
         self.curAction = 'D'
         self.untilRandom = -1
+        self.floydWarshall(inputMap)
+
+#----------Floyd-Warshall Algorithm-----------#
+    def floydWarshall(self, map):
+        self.maxFirst = 0
+        self.maxSecond = 0
+
+        #Find the dimensions of the board because they're given to us as 
+        #a goddamn dictionary
+        for i in map.iterkeys():
+            if i[0] > self.maxFirst:
+                self.maxFirst = i[0]
+            if i[1] > self.maxSecond:
+                self.maxSecond = i[1]
+        
+
+        #All-pairs shortest paths (The node at row,col is (row*maxCol)+col)
+        #Initiate array:
+        aBigNumber = self.maxFirst*self.maxSecond*self.maxFirst*self.maxSecond
+        self.allPairsShortestPath = []
+        for i in range(self.maxFirst*self.maxSecond):
+            self.allPairsShortestPath.append([])
+            for j in range(self.maxFirst*self.maxSecond):
+                self.allPairsShortestPath[i].append([])
+
+        #Initial Values:
+        for i in range(self.maxFirst*self.maxSecond):
+            for j in range(self.maxFirst*self.maxSecond):
+                if j == i-1 and not self.isWall(map[(self.firstCoord(j), self.secondCoord(j))]):
+                    self.allPairsShortestPath[i][j] = 1
+                elif j == i+1 and not self.isWall(map[(self.firstCoord(j), self.secondCoord(j))]):
+                    self.allPairsShortestPath[i][j] = 1
+                elif j == (i - self.maxFirst) and not self.isWall(map[(self.firstCoord(j), self.secondCoord(j))]):
+                    self.allPairsShortestPath[i][j] = 1
+                elif j == (i + self.maxFirst) and not self.isWall(map[(self.firstCoord(j), self.secondCoord(j))]):
+                    self.allPairsShortestPath[i][j] = 1
+                else:
+                    self.allPairsShortestPath[i][j] = aBigNumber
+
+        #Run algorithm:
+        for k in range(self.maxFirst*self.maxSecond):
+            if self.isWall(map[(self.firstCoord(k), self.secondCoord(k))]):
+                continue
+            print k
+            for i in range(self.maxFirst*self.maxSecond):
+                if self.isWall(map[(self.firstCoord(i), self.secondCoord(i))]):
+                    continue
+                for j in range(self.maxFirst*self.maxSecond):
+                    if self.isWall(map[(self.firstCoord(j), self.secondCoord(j))]):
+                        continue
+                    self.allPairsShortestPath[i][j] = min(self.allPairsShortestPath[i][j], \
+                        self.allPairsShortestPath[i][k] + self.allPairsShortestPath[k][j])
+#----------End really long block of code I hate Python---------#
+    def firstCoord(self, value):
+        return int(value / self.maxSecond)
+
+    def secondCoord(self, value):
+        return (value % self.maxSecond)
 
     def isWall(self, value):
         return (100<=value<=199)
