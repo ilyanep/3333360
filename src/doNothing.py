08fbd457dca25d12b1631fd876bae93817e77a96
Ilya Nepomnyashchiy <ilyanep@gmail.com>
2010-10-29 00:14:59 -0700

    Add an AI that does nothing

---
+++ src/doNothing.py
@@ -0,0 +1,9 @@
+import pygame, sys, os, random, math
+from pygame.locals import *
+
+class doNothingAI ():
+    def __init__(self, inputMap, curPos, rivalPos):
+       None
+
+    def think(self, curMap, curPos, rivalPos, curGraceTime):
+        None
